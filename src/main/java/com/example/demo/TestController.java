17bb515fc5e34b5a68edcfcbf62ad1739559e657
RoyalRaisins <215783288@qq.com>
2022-12-04 10:13:44 +0000

    Runnable on server

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.json.JSONObject;
 @RestController
 @RequestMapping("/test")
 public class TestController {
@@ -10,5 +12,13 @@ public class TestController {
     public String test(){
         System.out.println("test server requested");
         return "Test successful";
+    }
+    @PostMapping("/h")
+    public String rec(@RequestBody String json){
+        JSONObject msg = new JSONObject(json);
+        JSONObject body = msg.getJSONObject("notify_data").getJSONObject("body");
+        System.out.println("post request received, \nbody: \n" + json);
+        System.out.println("Body: \n"+body);
+        return "Post successful";
     }
 }
